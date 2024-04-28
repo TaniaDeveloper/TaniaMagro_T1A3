6c6661674b1d2a37fdc82fc5f49ca601762c4feb
TaniaDeveloper <tanialeanne2022@hotmail.com>
2024-04-28 14:21:14 +1000

    created address book with first input

---
+++ main.py
@@ -0,0 +1,12 @@
+# Address book application
+
+# Address Book Name
+print("My Address Book")
+
+# Address book input
+first_name = input("First name = ")
+last_name = input("Last name = ")
+mobile_number = input("Mobile number = ")
+email_address = input("Email address = ")
+address = input("Address = ")
+
