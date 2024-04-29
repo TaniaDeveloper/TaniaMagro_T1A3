b588382cf00cc3d34fe002e354610a27d21fdd59
TaniaDeveloper <tanialeanne2022@hotmail.com>
2024-04-29 20:05:36 +1000

    Updating readme.md and created start of console

--- main.py
+++ main.py
@@ -10,3 +10,28 @@ mobile_number = input("Mobile number = ")
 email_address = input("Email address = ")
 address = input("Address = ")
 
+def main():
+    initialize_database()
+
+    while True:
+        print("\nAddress Book List:")
+        print("1. Add Contact")
+        print("2. Search Contacts")
+        print("3. Update Contact")
+        print("4. Delete Contact")
+        print("5. Exit")
+
+        choice = input("Enter your choice (1-4): ")
+
+        if choice == "1":
+            add_contact()
+        elif choice == "2":
+            search_contact()
+        elif choice == "3":
+            update_contact()
+        elif choice == "4":
+            delete_contact()
+        elif choice == "5":
+            break
+        else:
+            print("Invalid choice")
---
+++ readme.md
@@ -0,0 +1,6 @@
+T1A3: Terminal Application
+
+Source Control Repository:
+
+Identify code style guide:  https://peps.python.org/pep-0008/
+                            
